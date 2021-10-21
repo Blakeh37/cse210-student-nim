86369c52876a7b677e5c5272b55010619e0155cd
blakeh37 <hollibe2000@gmail.com>
2021-10-20 22:39:06 -0600

    Writing all code for board.py

--- nim/game/board.py
+++ nim/game/board.py
@@ -1,3 +1,44 @@
 import random
 
-# TODO: Define the Board class here
+class Board:
+    
+    def __init__(self):
+        #list of numbers
+        self.piles = {}
+        self.__prepare()
+
+    def __prepare(self):
+        #turned the piles into a dictionary for better use 
+        self.num_piles = random.randint(2,5)
+        for pile in range(self.num_piles):
+            num_stones = random.randint(1,9)
+            stones = ('O ' * num_stones).split()
+            self.piles[pile] = stones
+
+    def to_string(self):
+        text = ''
+        for key in range(self.num_piles):
+            Os = ' '.join(self.piles[key])
+            text += (f'\n{key}: {Os}')
+        return text
+
+    def apply(self, move):
+        #change the board based on the move 
+        pile = move.get_pile()
+        stones = move.get_stones()
+
+        for i in range(stones):
+            self.piles[pile].pop()
+
+    def is_empty(self):
+        #checks if there is no more stones 
+        win_check = False
+
+        for key in range(self.num_piles):
+            if 'O' in self.piles[key]:
+                win_check = False
+            else:
+                win_check = True
+        return win_check
+
+
